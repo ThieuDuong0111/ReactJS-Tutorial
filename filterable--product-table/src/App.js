dc075a5dbead064dbdb68bf06feeb093baef4de7
thieu.duong <duongthieu1995@gmail.com>
2025-04-02 12:45:00 +0700

    Update App.js

--- filterable--product-table/src/App.js
+++ filterable--product-table/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ProductCategoryRow({ category }) {
 	return (
@@ -23,11 +23,19 @@ function ProductRow({ product }) {
 	);
 }
 
-function ProductTable({ products }) {
+function ProductTable({ products, filterText, inStockOnly }) {
 	const rows = [];
 	let lastCategory = null;
 
 	products.forEach((product) => {
+		if (
+			product.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1
+		) {
+			return;
+		}
+		if (inStockOnly && !product.stocked) {
+			return;
+		}
 		if (product.category !== lastCategory) {
 			rows.push(
 				<ProductCategoryRow
@@ -53,22 +61,49 @@ function ProductTable({ products }) {
 	);
 }
 
-function SearchBar() {
+function SearchBar({
+	filterText,
+	inStockOnly,
+	onFilterTextChange,
+	onInStockOnlyChange,
+}) {
 	return (
 		<form>
-			<input type='text' placeholder='Search...' />
+			<input
+				type='text'
+				value={filterText}
+				onChange={(e) => onFilterTextChange(e.target.value)}
+				placeholder='Search...'
+			/>
 			<label>
-				<input type='checkbox' /> Only show products in stock
+				<input
+					type='checkbox'
+					checked={inStockOnly}
+					onChange={(e) => onInStockOnlyChange(e.target.checked)}
+				/>{" "}
+				Only show products in stock
 			</label>
 		</form>
 	);
 }
 
 function FilterableProductTable({ products }) {
+	const [filterText, setFilterText] = useState("");
+	const [inStockOnly, setInStockOnly] = useState(false);
+
 	return (
 		<div>
-			<SearchBar />
-			<ProductTable products={products} />
+			<SearchBar
+				filterText={filterText}
+				inStockOnly={inStockOnly}
+				onFilterTextChange={setFilterText}
+				onInStockOnlyChange={setInStockOnly}
+			/>
+			<ProductTable
+				products={products}
+				filterText={filterText}
+				inStockOnly={inStockOnly}
+			/>
 		</div>
 	);
 }
